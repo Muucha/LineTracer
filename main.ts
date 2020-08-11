632f7e761683a32f39a1b6f58ab23f37ac036b1c
Muucha <54197234+Muucha@users.noreply.github.com>
2020-08-12 08:01:58 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
 let 速さ = 255
 basic.forever(function () {
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-    	
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 速さ)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
     } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
@@ -10,12 +8,8 @@ basic.forever(function () {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 速さ)
     }
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-        速さ = 20
-    } else {
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
         速さ = 255
+    } else {
+        速さ = 10
     }
 })
